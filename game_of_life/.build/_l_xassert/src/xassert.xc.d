bcb2fc30541a8bbb3ef3b3870494754361bcba89
Vansh Dassani <vd15808@my.bristol.ac.uk>
2016-10-28 14:58:16 +0100

    init skeleton

---
+++ game_of_life/.build/_l_xassert/src/xassert.xc.d
@@ -0,0 +1,4 @@
+.build/_l_xassert/src/xassert.xc.d .build/_l_xassert/src/xassert.xc.o .build/_l_xassert/src/xassert.xc.pca.xml: \
+  ../../Assignment/lib_xassert/src/xassert.xc \
+  ../../Assignment/lib_xassert/api/xassert.h
+
---
+++ game_of_life/.build/pca.d
@@ -0,0 +1,4 @@
+.build/_l_i2c/src/i2c_master_async.xc.o: ./src/main.xc
+.build/_l_xassert/src/xassert.xc.o: /home/fe15/vd15808/linux/Documents/CC/Assignment/lib_i2c/src/i2c_master_async.xc /home/fe15/vd15808/linux/Documents/CC/Assignment/lib_i2c/src/i2c_master_single_port.xc
+.build/_l_i2c/src/i2c_master.xc.o: /home/fe15/vd15808/linux/Documents/CC/Assignment/lib_i2c/src/i2c_master_async.xc ./src/main.xc
+.build/_l_i2c/src/i2c_slave.xc.o: /home/fe15/vd15808/linux/Documents/CC/Assignment/lib_i2c/src/i2c_master_async.xc ./src/main.xc /home/fe15/vd15808/linux/Documents/CC/Assignment/lib_i2c/src/i2c_master_ext.xc /home/fe15/vd15808/linux/Documents/CC/Assignment/lib_i2c/src/i2c_master_single_port.xc /home/fe15/vd15808/linux/Documents/CC/Assignment/lib_i2c/src/i2c_master.xc
